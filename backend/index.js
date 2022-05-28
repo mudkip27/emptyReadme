47a1f7b082d6ff024d616210111e91090fbce513
Emmet Ocampo <emmetocampo@gmail.com>
2022-05-28 22:49:13 +0000

    RestGetCode

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // app.get()
 // app.post()
 // app.put()
 // app.delete()
 
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+];
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!!!!!!!!!!!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000...');
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('The course with the given id was not found');
+    }
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
 });
